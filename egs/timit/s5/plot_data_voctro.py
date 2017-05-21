7768c5726de7a4965422522c20026cdf56a32f25
pabloEntropia <pablo.alonso@upf.edu>
2017-05-21 16:00:45 +0200

    added color bar

--- egs/timit/s5/plot_data_voctro.py
+++ egs/timit/s5/plot_data_voctro.py
@@ -11,54 +11,49 @@ fn_features = sys.argv[1]
 
 
 # load ASCII file
-fpgp = open(fn_features) 
+fpgp = open(fn_features)
 ll = fpgp.readlines()
 fpgp.close
 
 figs_folder = sys.argv[2] + '/plots'
 if not os.path.exists(figs_folder):
-	os.mkdir(figs_folder)
-	os.chown(figs_folder,1000 , 1000)
+    os.mkdir(figs_folder)
+    os.chown(figs_folder,1000 , 1000)
 
 data_folder = sys.argv[2] + '/data'
 if not os.path.exists(data_folder):
-	os.mkdir(data_folder)
-	os.chown(data_folder, 1000, 1000)
+    os.mkdir(data_folder)
+    os.chown(data_folder, 1000, 1000)
 
 # convert ASCII data to numpy format matrix for one file. (skip first row)
 pgp_files = [];
 frames = np.array([])
 for l in ll:
-	# check new files
-	if l.find(" [")>0:
-		new_file = {'fname': l.split()[0], 'PGP': 0}
-		pgp_files.append(new_file)
-		frames = np.array([])
-		print 'Saving PGP data for file:' + new_file['fname']
-		continue
+    # check new files
+    if l.find(" [")>0:
+        new_file = {'fname': l.split()[0], 'PGP': 0}
+        pgp_files.append(new_file)
+        frames = np.array([])
+        print 'Saving PGP data for file:' + new_file['fname']
+        continue
     # accumulate new lines as new feature frames
-	l2 = l.replace("]","")
-	vals = np.array(l2.split()).astype(float)
-	if frames.size == 0:
-		frames = vals
-	else:
-		frames = np.vstack((frames, vals) )
-	#print frames.shape
-	# check end of file and store results
-	if l.find("]") > 0:
-		
-		# save numpy
-		pgp_files[-1]['PGP'] = frames
-		fname = os.path.join(data_folder, pgp_files[-1]["fname"]+'.npy')	
-		np.save(fname,frames)
-		# save plot 
-		plt.matshow(frames.T)
-		plt.title('PGP per frame / ' + pgp_files[-1]["fname"])
-		fname = os.path.join(figs_folder, pgp_files[-1]["fname"]+'.png')
-		plt.savefig(fname)
-		plt.close()
+    l2 = l.replace("]","")
+    vals = np.array(l2.split()).astype(float)
+    if frames.size == 0:
+        frames = vals
+    else:
+        frames = np.vstack((frames, vals) )
+    # check end of file and store results
+    if l.find("]") > 0:
 
-
-
-
-
+        # save numpy
+        pgp_files[-1]['PGP'] = frames
+        fname = os.path.join(data_folder, pgp_files[-1]["fname"]+'.npy')
+        np.save(fname,frames)
+        # save plot
+        plt.matshow(frames.T)
+        plt.title('PGP per frame / ' + pgp_files[-1]["fname"])
+        fname = os.path.join(figs_folder, pgp_files[-1]["fname"]+'.png')
+        plt.colorbar()
+        plt.savefig(fname)
+        plt.close()
